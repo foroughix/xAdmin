64ed886bf663a68c896f16a023400bcc6dde33af
foroughinia18 <85922868+foroughinia18@users.noreply.github.com>
2021-12-04 10:28:16 +0330

    Update fxmanifest.lua

--- xAdmin/fxmanifest.lua
+++ xAdmin/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'foroughinia18#1533'
-description 'xAdmin'
+description 'xAdmin - FiveM admin system based by Commands'
 version '1.0'
 
 client_script 'client.lua'
